ad1b66e51c74ee808484afac0df1f1235dc903a9
Rian Mendonça do Vale <111614161+ps1ck@users.noreply.github.com>
2025-11-04 10:42:38 -0300

    v2

--- src/js/formulario.js
+++ src/js/formulario.js
@@ -1,5 +1,5 @@
 /* ===== CONFIG ===== */
-const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwxcmTPpAShVhJNCTlw4rrgi_8ZE2LgGqHAMLrhhe5FXlmr9b-k4ofQhVTByET8Xhz-/exec'; // ex: https://script.google.com/macros/s/AKfy.../exec
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwShWr_oM09dLdfJF97bn3bYrFzGqlovqwIf-kzSySiYgOxAJ5MTcC3IwbGTy02nidc/exec';
 const WEB_APP_TOKEN = 'meu_token_5j3kL9';
 /* ================== */
 
